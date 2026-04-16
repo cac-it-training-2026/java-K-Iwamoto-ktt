63d1ef0c438f3245acafe2552dad33b6732f5a70
k-iwamoto <k-iwamoto@CD26302L.arksystems.co.jp>
2026-04-16 14:12:13 +0900

    基礎編 完了

--- java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_1.java
@@ -10,7 +10,14 @@ public class Question04_1 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
+		int[] age_Group = new int[5];
+		age_Group[0] = 12;
+		age_Group[1] = 18;
+		age_Group[2] = 24;
+		age_Group[3] = 19;
+		age_Group[4] = 20;
+
+		System.out.println("3番目の人の年齢は" + age_Group[2] + "歳です。");
 
 	}
 
